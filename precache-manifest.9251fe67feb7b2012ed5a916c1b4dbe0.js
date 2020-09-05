22483ae957795e19685b79b6a9537033985e6ea9
Mohamed-HAZAMI <meed.hazami@gmail.com>
2020-09-05 14:31:41 +0100

    Updates

---
+++ precache-manifest.9251fe67feb7b2012ed5a916c1b4dbe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ac4d397397d830d56dce1c52229901",
+    "url": "/simple-navbar/index.html"
+  },
+  {
+    "revision": "c332764aca9bd082afcf",
+    "url": "/simple-navbar/static/css/main.66124c9b.chunk.css"
+  },
+  {
+    "revision": "3665cf7bb9221bac4931",
+    "url": "/simple-navbar/static/js/2.892a179a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-navbar/static/js/2.892a179a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c332764aca9bd082afcf",
+    "url": "/simple-navbar/static/js/main.41dd9222.chunk.js"
+  },
+  {
+    "revision": "0e47d77868c4c04d032c",
+    "url": "/simple-navbar/static/js/runtime-main.5ee6130a.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/simple-navbar/static/media/react_logo.580ffd26.png"
+  }
+]);
