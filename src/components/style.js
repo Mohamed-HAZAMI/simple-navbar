3efedf0027b12b9f48f4497735a87f32b888865c
Mohamed-HAZAMI <meed.hazami@gmail.com>
2020-09-07 14:15:59 +0100

    add animation navbar

--- src/components/style.js
+++ src/components/style.js
@@ -1,51 +1,61 @@
 import styled from "styled-components";
 
 export const Style=styled.div`
-background-color: rgb(75, 75, 75);
 min-width:100%;
-height:80px;
+height:100vh;
 section {
-    width:80%;
-    height:80px;
-    margin:auto;
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
+background-color: rgb(75, 75, 75);
+height:10vh;
+display:flex;
+justify-content:space-around;
+align-items:center;
+margin:0 ;
+padding:0;
+box-sizing: border-box;
 }
 ul {
     ${props => {
     if (props.matches) {
       return `
         display: flex;
-
     `
     } else if (props.click) {
       return `
-        display:flex;
+        margin:0 ;
+        padding:0;
+        box-sizing: border-box;
+        height: 90vh;
+        display: flex;
+        justify-content:space-around;
         flex-direction: column;
-        height: 250px;
         min-width:100%;
         padding-left:0;
         position: absolute;
-        top:55px;
+        top:10vh;
         right: 0;
         background-color: rgb(75, 75, 75);
-        transition: transform 0.5s ease-in;
-        transform: translateX(200%);
+        transition: clip-path 0.5s ease-in;
+        // transform: translateX(200%);
+        clip-path: circle(0px at 100% 0%);
     `
     } else {
         return `
-        display:flex;
+        margin:0 ;
+        padding:0;
+        box-sizing: border-box;
+        height: 90vh;
+        display: flex;
+        justify-content:space-around;
         flex-direction: column;
-        height: 250px;
         min-width:100%;
         padding-left:0;
         position: absolute;
-        top:55px;
+        top:10vh;
         right: 0;
         background-color: rgb(75, 75, 75);
-        transform: translateX(0%);
-        transition: transform 0.5s ease-in;
+        // transform: translateX(0%);
+        transition: clip-path 0.5s ease-in;
+        clip-path: circle(1000px at 100% 0%);
     `
     }
 }};
@@ -55,12 +65,14 @@ ul {
 @keyframes li {
  from{
   padding:20px;
+  font-size:20px;
   color:#fff;
   opacity:0; 
   transform:translateX(200%); 
  }
  to{
   padding:20px;
+  font-size:20px;
   color:#fff;
   opacity:1; 
   transform:translateX(0%); 
@@ -71,21 +83,25 @@ li:nth-child(1) {
     animation: ${props => props.click ?"none" : `li 1s` };
     padding:20px;
     color:#fff;
+    font-size:20px;
 }
 li:nth-child(2) {
     animation: ${props => props.click ?"none" : `li 2s` };
     padding:20px;
     color:#fff;
+    font-size:20px;
 }
 li:nth-child(3) {
     animation: ${props => props.click ?"none" : `li 3s` };
     padding:20px;
     color:#fff;
+    font-size:20px;
 }
 li:nth-child(4) {
     animation: ${props => props.click ?"none" : `li 4s` };
     padding:20px;
     color:#fff;
+    font-size:20px;
 }
 img {
     width:80px;
